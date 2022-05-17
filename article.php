e18781bd63f7c1016e152f39d8a35b020a88172b
TugsB <tugs_247@yahoo.com>
2022-05-17 17:49:11 +0200

    Prvi dan tecaja

--- article.php
+++ article.php
@@ -1,3 +1,7 @@
 <?php
-include 'helpers/posts.php';
+$posts = include 'helpers/posts.php';
 var_dump($_GET);
+$id = isset($_GET['id'])?$_GET['id']:FALSE;
+if($_GET["id"] > count($posts)-1){
+    header('Location: 404.php');
+};
